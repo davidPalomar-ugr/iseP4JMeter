5af8433b536afb52c0c4bb245c19f70ec541b116
David Palomar <david.palomar@bestsecret.com>
2018-10-03 14:18:39 +0200

    Añado un script basico para probar el entorno con Curl

---
+++ pruebaEntorno.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TOKEN=$(curl -s \
+-u etsiiApi:laApiDeLaETSIIDaLache \
+-d "login=mariweiss@tropoli.com&password=anim" \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-X POST http://localhost:3000/api/v1/auth/login)
+
+resultado=$?
+if test "$resultado" != "0"; then
+   echo "ERROR: Curl fallo con resultado: $resultado"
+fi
+
+curl \
+-H "Authorization: Bearer $TOKEN" \
+http://localhost:3000/api/v1/alumnos/alumno/mariweiss%40tropoli.com 
