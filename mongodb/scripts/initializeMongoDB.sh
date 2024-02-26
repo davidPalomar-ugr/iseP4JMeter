f513a10990d90ff970ebfdcae855ff5d9ebf2db4
David Palomar <davidpalomarsaez@gmail.com>
2024-02-26 22:52:29 +0100

    Unifica version de Mongo a 6
    Fija version 6 tanto para la instancia de ejecución como
    para el script de inicialización.

--- mongodb/scripts/initializeMongoDB.sh
+++ mongodb/scripts/initializeMongoDB.sh
@@ -3,12 +3,12 @@ mongosh mongodb/etsii --eval 'db.usuarios.drop();'
 
 mongoimport --host mongodb --db etsii --collection alumnos --type json --file generated_alumnos.json --jsonArray
 
+mongoimport --host mongodb --db etsii --collection usuarios --type json --file generated_admin.json --jsonArray
+
 mongosh mongodb/etsii --eval 'db.alumnos.createIndex({"email":1},{"unique":1});'
 
 mongosh mongodb/etsii --eval 'db.alumnos.find({},{usuarioObj:1,_id:0}).forEach(function(d){db.usuarios.insert((d.usuarioObj))});'
 
-mongosh mongodb/etsii --eval 'db.alumnos.update({}, {$unset: {usuarioObj: 1}}, {multi: true});'
+mongosh mongodb/etsii --eval 'db.alumnos.updateMany({}, {$unset: {usuarioObj: 1}}, {multi: true});'
 
 mongosh mongodb/etsii --eval 'db.usuarios.createIndex({"id":1},{"unique":1});'
-
-mongoimport --host mongodb --db etsii --collection usuarios --type json --file generated_admin.json --jsonArray
